c913e0503f0201920367c790c3e359d36b80c05f
Stariel Isaac <starielisaac@gmail.com>
2023-05-08 19:10:49 -0300

    refactor:  class Character  - 'src/Races/Character.ts'

--- src/Character.ts
+++ src/Character.ts
@@ -3,7 +3,7 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
-import getRandomInt from './utils';
+// import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _race : Race;
@@ -31,6 +31,35 @@ export default class Character implements Fighter {
     };
   }
 
+  get race() : Race {
+    return this._race;
+  }
+
+  get archetype() : Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints() : number {
+    return this._lifePoints;
+  }
+
+  get strength() : number {
+    return this._strength;
+  }
+
+  get defense() : number {
+    return this._defense;
+  }
+
+  get dexterity() : number {
+    return this._dexterity;
+  }
+
+  get energy() : Energy {
+    return { ...this._energy };
+  }
+
+  // metodos gerais
   attack(enemy: Fighter): void {
     enemy.receiveDamage(this._strength);
   }
@@ -40,10 +69,10 @@ export default class Character implements Fighter {
   }
 
   levelUp(): void {
-    this._maxLifePoints += getRandomInt(1, 10);
-    this._strength += getRandomInt(1, 10);
-    this._dexterity += getRandomInt(1, 10);
-    this._defense += getRandomInt(1, 10);
+    this._maxLifePoints += Character.randomNumber();
+    this._strength += Character.randomNumber();
+    this._dexterity += Character.randomNumber();
+    this._defense += Character.randomNumber();
     this._energy.amount = 10;
 
     if (this._maxLifePoints > this._race.maxLifePoints) {
@@ -65,34 +94,6 @@ export default class Character implements Fighter {
       this._lifePoints = -1;
     }
     return this._lifePoints;
-  }
-
-  public race() : Race {
-    return this._race;
-  }
-
-  public archetype() : Archetype {
-    return this._archetype;
-  }
-
-  public get lifePoints() : number {
-    return this._lifePoints;
-  }
-
-  public get strength() : number {
-    return this._strength;
-  }
-
-  public get defense() : number {
-    return this._defense;
-  }
-
-  public dexterity() : number {
-    return this._dexterity;
-  }
-
-  public get energy() : Energy {
-    return { ...this._energy };
   }
 
   static randomNumber(): number {
