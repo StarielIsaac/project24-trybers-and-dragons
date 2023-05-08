fed9e0c60359d3438e1565c16f3a3b90e25fa66c
Stariel Isaac <starielisaac@gmail.com>
2023-05-08 08:52:44 -0300

    chore: Add comments

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,11 +1,14 @@
 import { EnergyType } from '../Energy';
 
+// Declaração da classe abstrata Archetype
 export default abstract class Archetype {
+  // Declaração dos atributos privados _name, _special, _cost e estático _instanceCount
   private _name : string;
   private _special : number;
   private _cost : number;
   private static _instanceCount = 0;
 
+  // Construtor da classe, recebe um nome e inicializa os atributos _name, _special e _cost
   constructor(name : string) {
     this._name = name;
     this._special = 0;
@@ -13,22 +16,26 @@ export default abstract class Archetype {
     Archetype._instanceCount += 1;
   }
 
+  // Método público que retorna o nome do arquétipo
   public name() : string {
     return this._name;
   }
 
+  // Método público que retorna o valor do atributo _special
   public special() : number {
     return this._special;
   }
 
+  // Método público que retorna o valor do atributo _cost
   public cost() : number {
     return this._cost;
   }
 
+  // Método estático que retorna a quantidade de instâncias criadas da classe Archetype
   static createdArchetypeInstances() : number {
     throw new Error('Not implemented');
   }
 
-  // corresponde ao tipo de energia que este arquétipo deve ter (mana ou stamina)
+  // Método abstrato que retorna o tipo de energia que o arquétipo deve ter (mana ou stamina)
   abstract get energyType() : EnergyType;
 }
