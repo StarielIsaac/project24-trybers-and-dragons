ef2b68bf166a60cf8d5e310ca8bdff04d51564c0
Stariel Isaac <starielisaac@gmail.com>
2023-05-09 18:31:40 -0300

    feat: created method figth in class PVE - src/Battle/PVE.ts

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -13,4 +13,28 @@ export default class PVE extends Battle {
     this._player = player;
     this._enemy = monsters;
   }
+
+  attackCharacter(monster: SimpleFighter): void {
+    this._player.attack(monster);
+  }
+  
+  attackMonster(monster: SimpleFighter): void {
+    monster.attack(this._player);
+  }
+
+  // verifica se pelo menos um dos lutador/personagem/monstro ainda está vivo.
+  areFightersAlive(): boolean {
+    return this._player.lifePoints > 0 && this._enemy
+      .some((monster) => monster.lifePoints > 0);
+  }
+   
+  fight(): number {
+    while (this.areFightersAlive()) {
+      this._enemy.forEach((monster) => {
+        this.attackCharacter(monster);
+        this.attackMonster(monster);
+      });
+    }
+    return super.fight();
+  }
 }
