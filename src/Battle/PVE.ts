31358dcc0598bd87ac25aa650e6acf6b746346cd
Stariel Isaac <starielisaac@gmail.com>
2023-05-09 18:29:22 -0300

    refactor: interface Fighter extends SimpleFighter

--- src/Battle/PVE.ts
+++ src/Battle/PVE.ts
@@ -1,13 +1,16 @@
-import Battle from './Battle';
 import Fighter, { SimpleFighter } from '../Fighter';
 import Monster from '../Monster';
+import Battle from './Battle';
 
 export default class PVE extends Battle {
   private _player: Fighter;
-  private _enemy: Monster[] | Fighter | SimpleFighter;
-  constructor(player1: Fighter, enemy: Monster[] | Fighter | SimpleFighter) {
-    super(player1);
-    this._player = player1;
-    this._enemy = enemy;
+  private _enemy: SimpleFighter[] | Monster[];
+  constructor(
+    player: Fighter,
+    monsters: SimpleFighter[] | Monster[],
+  ) {
+    super(player);
+    this._player = player;
+    this._enemy = monsters;
   }
 }
